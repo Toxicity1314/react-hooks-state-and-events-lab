11eb70833d1f9757444b4ca0c077244ba4e5ce08
Sean Welsh <s.welsh1314@gmail.com>
2022-12-14 13:46:46 -0500

    commit tree 6a2012332b6757c0a69356d49a4e0cd355a2c63a

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false)
+  
+  function handleButtonClick(){
+    setInCart(!inCart)
+  }
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" :""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        className={inCart ? "remove" :"add"}
+        onClick = {handleButtonClick}>{inCart ? "remove from cart" :"Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filterBy, setFilterBy] = useState("")
+  const filteredItems = items.filter(item => item.category.includes(filterBy))
+  const filteredItemsToDisplay = filteredItems.map((item) => (
+    <Item key = {item.id} {...item} />
+  ))
+
+  function handleFilterChange(e){
+    setFilterBy(e.target.value)
+   
+  }
+  console.log(filterBy)
+
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange = {handleFilterChange}>
+          <option value="">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {filteredItemsToDisplay}
       </ul>
     </div>
   );
